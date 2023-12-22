e489bdfb24877b68c1114aa7aa6f3dc9541f535c
rittin2803 <rittin2803@gmail.com>
2023-12-22 15:59:01 +0530

    Path, FS modules

---
+++ reference/path_demo.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+//Base file
+console.log(path.basename(__filename));
+
+//Directory
+console.log(path.dirname(__filename));
+
+//Extension
+console.log(path.extname(__filename));
